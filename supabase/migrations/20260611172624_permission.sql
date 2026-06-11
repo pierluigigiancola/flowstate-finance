8a402083e7a4384a991197d306342a8dd46bb85e
Pierluigi Giancola <pierluigi.giancola@gmail.com>
2026-06-11 19:26:54 +0200

    grant permission

---
+++ supabase/migrations/20260611172624_permission.sql
@@ -0,0 +1,5 @@
+-- Grant permissions
+grant usage on schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all tables in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all sequences in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all functions in schema public to postgres, anon, authenticated, service_role;
